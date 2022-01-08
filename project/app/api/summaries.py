cb13a9d96332af05d50393771cf7bb84e83ac7f3
dudeperf3fct <dudeperf3ct@users.noreply.github.com>
2022-01-08 18:22:55 +0530

    add upto part 1, chapter 8

---
+++ project/app/api/summaries.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, HTTPException
+from typing import List
+
+from app.api import crud
+from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
+from app.models.tortoise import SummarySchema
+
+
+router = APIRouter()
+
+
+@router.post("/", 
+            response_model=SummaryResponseSchema, 
+            summary="Create a summary",
+            description="Add created summaries to database",
+            status_code=201)
+async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+    summary_id = await crud.post(payload)
+
+    response_object = {
+        "id": summary_id,
+        "url": payload.url
+    }
+    return response_object
+
+
+@router.get("/{id}/",
+            response_model=SummarySchema,
+            summary="Read particular summary with given id",
+            status_code=200)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    return summary
+
+
+@router.get("/", 
+            response_model=List[SummarySchema],
+            summary="Read all summaries in database",
+            status_code=200)
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
